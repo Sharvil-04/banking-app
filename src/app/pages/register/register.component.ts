f1561f2d54f2ee7fe59bf4336967e133fbf4d97c
Sharvil-04 <sharvil12345patel@gmail.com>
2024-08-16 13:18:36 -0400

    Register page added

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+    })
+  }
+
+  onSubmit(){
+    alert('Registeration Successful')
+    this.router.navigate(['/home']);
+    
+  }
 
 }
